49d6bf03f4127cbd7750f6ad2581241189f776a1
Cj <srikrishna.cj@gmail.com>
2021-11-16 10:56:36 +0530

    Spring Initializr Checkout

---
+++ src/main/java/com/example/mynewboot/MynewbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynewboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynewbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MynewbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mynewboot/MynewbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynewboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynewbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
